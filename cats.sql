d2012d697e9e6c60782533c33eecafae934a99c9
Phil <philwekullo@gmail.com>
2024-03-12 10:04:27 +0300

    complete

---
+++ cats.sql
@@ -0,0 +1,4 @@
+INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+INSERT INTO cats (name, age, breed) VALUES ('Hana', 1, 'Tabby');
+INSERT INTO cats (name, age, breed) VALUES ('Nona', 4, 'Tortoiseshell');
+INSERT INTO cats (name, age, breed) VALUES ('Lil Bub', 2, 'perma-kitten');
